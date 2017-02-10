8419ade09858fc1d8cc6a61a769657b2a3457874
Chris Schultz <chris@Chriss-MacBook-Pro.local>
2017-02-10 16:52:39 -0600

    medicine extractor now returns extracted medicines

--- src/data/medicine.py
+++ src/data/medicine.py
@@ -1,14 +1,16 @@
 class Medicine(object):
     def __init__(self, identification_name):
-        self.identification_name = indentification_name
+        self.identification_name = identification_name
         self.brand_names = set()
         self.dosages = []
 
-    def __hash__(self):
-        hash(self.identification_name)
+    def __repr__(self):
+        return self.identification_name
 
     def add_dosage(self, dosage_type_str, dosage_amount_str):
-        self.dosages.append((dosage_type_str, dosage_amount_str))
+        if dosage_type_str and dosage_amount_str:
+            self.dosages.append((dosage_type_str, dosage_amount_str))
 
-    def add_brand_names(self, brand_name):
-        self.brand_names.add(brand_names)
+    def add_brand_name(self, brand_name):
+        if brand_name:
+            self.brand_names.add(brand_name)
--- src/extractors/medicine_extractors.py
+++ src/extractors/medicine_extractors.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+from src.data.medicine import Medicine
 from src.extractors.info_extractor_interface import IInfoExtractor
 
 
@@ -27,8 +28,8 @@ class MedexExtractor(IInfoExtractor):
         # read in output from medex and store results
         medicines_in_reports = []
         for i in xrange(len(medical_reports)):
-            medicines_in_this_report = {}
-            medicines_in_reports.append(medicines_in_this_report)
+            medicines_in_this_report_dict = {}
+            medicines_in_reports.append(medicines_in_this_report_dict)
             with open('libs/Medex_UIMA_1.2.1/output/report_' + str(i) + '.txt', 'r') as output_file:
                 for medicine_tagged_line in output_file:
                     split_medicine_line = medicine_tagged_line.split('|')
@@ -38,9 +39,28 @@ class MedexExtractor(IInfoExtractor):
                     brand_name = split_medicine_line[1].strip()
                     brand_name = brand_name[:brand_name.find('[')]
 
+                    dosage_type = split_medicine_line[3].strip()
+                    dosage_amount = split_medicine_line[4].strip()
+
+                    if medicine_name:
+                        medicine_indentification_name = medicine_name
+                    elif brand_name:
+                        medicine_indentification_name = brand_name
+
+                    if not medicine_indentification_name in medicines_in_this_report_dict:
+                        medicine = Medicine(medicine_indentification_name)
+                        medicines_in_this_report_dict[medicine_indentification_name] = medicine
+                    else:
+                        medicine = medicines_in_this_report_dict[medicine_indentification_name]
+
+                    medicine.add_brand_name(brand_name)
+                    medicine.add_dosage(dosage_type, dosage_amount)
+
         # clean up medex input and output files
         for i in xrange(len(medical_reports)):
             os.remove('libs/Medex_UIMA_1.2.1/input/report_' + str(i) + '.txt')
             os.remove('libs/Medex_UIMA_1.2.1/output/report_' + str(i) + '.txt')
             os.remove('libs/Medex_UIMA_1.2.1/log/report_' + str(i) + '.txt')
             os.remove('libs/Medex_UIMA_1.2.1/sents/report_' + str(i) + '.txt')
+
+        return medicines_in_reports
