b3b3420ea74fbc871b6ec75ce50e14786ba10e4a
Chris Schultz <chris@Chriss-MacBook-Pro.local>
2017-02-12 17:48:59 -0600

    added medical history to patient

---
+++ src/data/medical_history.py
@@ -0,0 +1,19 @@
+class MedicalHistory(object):
+    '''
+    Stores a patient's medical history
+    '''
+    def __int__(self):
+        self.entries = {}
+
+    def add_entry(date, entry_details_dict):
+        '''
+        Adds to entry ro medical history
+
+        date should be a datetime object
+        entry_details_dict should be a dictionary such as:
+        {
+          medicine: [medicine_obj_0, medicine_obj_1]
+          doctors: ['Dr. Somebody', 'Dr. Who']
+        }
+        '''
+        self.entries[str(date)] = entry_details_dict
--- src/data/patient.py
+++ src/data/patient.py
@@ -1,8 +1,12 @@
+from src.data.medical_history import MedicalHistory
+
+
 class Patient(object):
     def __init__(self, name, age=-1, gender='unknown'):
         self.name = name
         self.age = -1
         self.gender = gender
+        self.medical_history = MedicalHistory()
 
     def __repr__(self):
         return self.name
