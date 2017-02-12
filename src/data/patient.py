171693a95cc9ef2e82e9520e17c9cb89373c42f4
Chris Schultz <chris@Chriss-MacBook-Pro.local>
2017-02-12 17:53:17 -0600

    added medical history to patient

--- src/data/patient.py
+++ src/data/patient.py
@@ -2,11 +2,11 @@ from src.data.medical_history import MedicalHistory
 
 
 class Patient(object):
-    def __init__(self, name, age=-1, gender='unknown'):
+    def __init__(self, name, age=-1, gender='unknown', previous_medical_history = MedicalHistory()):
         self.name = name
         self.age = -1
         self.gender = gender
-        self.medical_history = MedicalHistory()
+        self.medical_history = previous_medical_history
 
     def __repr__(self):
         return self.name
