f6c0867372471e6d2fc4d77269bbecdb1cbde531
Chris Schultz <chris@Chriss-MacBook-Pro.local>
2017-02-13 08:27:18 -0600

    fixed medical history being the same object for every patient

--- src/data/patient.py
+++ src/data/patient.py
@@ -2,11 +2,14 @@ from src.data.medical_history import MedicalHistory
 
 
 class Patient(object):
-    def __init__(self, name, age=-1, gender='unknown', previous_medical_history=MedicalHistory()):
+    def __init__(self, name, age=-1, gender='unknown', previous_medical_history=None):
         self.name = name
         self.age = -1
         self.gender = gender
-        self.medical_history = previous_medical_history
+        if previous_medical_history:
+            self.medical_history = previous_medical_history
+        else:
+            self.medical_history = MedicalHistory()
 
     def __repr__(self):
         return self.name
